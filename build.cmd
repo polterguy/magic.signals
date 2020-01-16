367945c3f9c204fe15dc71dd9f9344fa05fbd8de
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 17:27:15 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,3 +1,11 @@
+
+set version=%1
+set key=%2
+
 cd %~dp0
+
 dotnet build magic.signals.contracts/magic.signals.contracts.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet nuget push magic.signals.contracts/bin/Release/magic.signals.contracts.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
+
 dotnet build magic.signals.services/magic.signals.services.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet nuget push magic.signals.services/bin/Release/magic.signals.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
