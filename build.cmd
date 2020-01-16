3ffebe406f6ff4a571090e2cfd4caa06efce4ade
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 16:26:30 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 cd %~dp0
-dotnet build magic.signals.contracts/magic.signals.contracts.csproj --configuration Release
-dotnet build magic.signals.services/magic.signals.services.csproj --configuration Release
+dotnet build magic.signals.contracts/magic.signals.contracts.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet build magic.signals.services/magic.signals.services.csproj --configuration Release --source https://api.nuget.org/v3/index.json
